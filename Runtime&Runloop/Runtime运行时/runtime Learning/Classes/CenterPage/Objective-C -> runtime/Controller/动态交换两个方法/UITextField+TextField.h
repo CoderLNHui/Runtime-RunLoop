ad126cae847ecf8d441964e4fa7cbd61d1ad9614
CustomPBWaters <liunan_615@sina.com>
2017-11-10 11:10:53 +0800

    no message

--- Runtime&Runloop/Runtime运行时/runtime Learning/Classes/CenterPage/Objective-C -> runtime/Controller/动态交换两个方法/UITextField+TextField.h
+++ Runtime&Runloop/Runtime运行时/runtime Learning/Classes/CenterPage/Objective-C -> runtime/Controller/动态交换两个方法/UITextField+TextField.h
@@ -26,7 +26,7 @@
 
 
 /*
- *【方式三：runtime交换方法(修改UITextField的占位文字，且设置文字和文字颜色是无序的)】
+ *【runtime交换方法(修改UITextField的占位文字，且设置文字和文字颜色是无序的)】
  * @param placeholder       占位文字
  */
 - (void)setLN_Placeholder:(NSString *)placeholder;
